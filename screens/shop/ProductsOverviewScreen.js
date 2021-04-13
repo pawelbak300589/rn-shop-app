f916051883ad33a712486ba7e6f3d485ccda6702
Pawel Bak <pawelbak300589@gmail.com>
2021-04-13 19:52:04 +0100

    Update product detail screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -8,7 +8,24 @@ const ProductsOverviewScreen = (props) => {
   const products = useSelector(state => state.products.availableProducts);
 
   return (
-    <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <ProductItem image={itemData.item.imageUrl} title={itemData.item.title} price={itemData.item.price} onViewDetail={() => { }} onAddToCart={() => { }} />} />
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate('ProductDetail', {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            })
+          }}
+          onAddToCart={() => { }}
+        />
+      )}
+    />
   );
 };
 
