8e4efe5ed226e0567100f5334e134323ff36d5ac
Pawel Bak <pawelbak300589@gmail.com>
2021-04-13 18:22:46 +0100

    Add store and products files for redux

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+import productReducer from './store/reducers/products';
+
+const rootReducer = combineReducers({
+  product: productReducer
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <View>
-      <Text>A new app!</Text>
-    </View>
+    <Provider store={store}>
+      <View>
+        <Text>A new app!</Text>
+      </View>
+    </Provider>
   );
 }
