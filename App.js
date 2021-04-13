df6616c00116b6a4842d2dd178089a02f0b39e7d
Pawel Bak <pawelbak300589@gmail.com>
2021-04-13 19:18:36 +0100

    Add shop navigator to the App

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
-import productReducer from './store/reducers/products';
+import productsReducer from './store/reducers/products';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  product: productReducer
+  products: productsReducer
 });
 
 const store = createStore(rootReducer);
@@ -14,9 +14,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>A new app!</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
