2dd5bccdd4c1bf63e91a68ad3907a254cac7b206
Pawel Bak <pawelbak300589@gmail.com>
2021-04-15 20:57:27 +0100

    Add header button for cart in the overview screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { StyleSheet, FlatList, View, Text } from 'react-native';
+import { StyleSheet, FlatList, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import HeaderButton from '../../components/UI/HeaderButton';
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector(state => state.products.availableProducts);
@@ -34,7 +36,10 @@ const ProductsOverviewScreen = (props) => {
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products'
+  headerTitle: 'All Products',
+  headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    <Item title='Cart' iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} onPress={() => { }} />
+  </HeaderButtons>
 };
 
 const styles = StyleSheet.create({});
