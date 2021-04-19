d170d08f67555af07beddb56051d0d4ea161e758
Pawel Bak <pawelbak300589@gmail.com>
2021-04-19 18:16:08 +0100

    Add cart header button to products overview screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -35,11 +35,16 @@ const ProductsOverviewScreen = (props) => {
   );
 };
 
-ProductsOverviewScreen.navigationOptions = {
-  headerTitle: 'All Products',
-  headerRight: <HeaderButtons HeaderButtonComponent={HeaderButton}>
-    <Item title='Cart' iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'} onPress={() => { }} />
-  </HeaderButtons>
+ProductsOverviewScreen.navigationOptions = navData => {
+  return {
+    headerTitle: 'All Products',
+    headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+      <Item title='Cart' iconName={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+        onPress={() => {
+          navData.navigation.navigate('Cart')
+        }} />
+    </HeaderButtons>
+  }
 };
 
 const styles = StyleSheet.create({});
