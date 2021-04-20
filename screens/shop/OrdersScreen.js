bcf3f73a81a0e2c8cc505a571c03d045398a5d88
Pawel Bak <pawelbak300589@gmail.com>
2021-04-20 20:12:27 +0100

    Create order item and use it on the order screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,12 +4,13 @@ import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
 
 const OrdersScreen = (props) => {
   const orders = useSelector(state => state.orders.orders);
 
   return (
-    <FlatList data={orders} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>} />
+    <FlatList data={orders} keyExtractor={item => item.id} renderItem={itemData => <OrderItem amount={itemData.item.totalAmount} date={itemData.item.readableDate} />} />
   );
 };
 
