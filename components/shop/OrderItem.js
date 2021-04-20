f4511786fb1fd02a345f038641d6d7c5a169e86a
Pawel Bak <pawelbak300589@gmail.com>
2021-04-20 20:26:51 +0100

    Make the "show details" button work

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 
 import Colors from '../../constants/Colors';
 import CartItem from './CartItem';
 
 const OrderItem = (props) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.totalAmount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
-      <Button color={Colors.primary} title="Show Details" onPress={() => { }} />
+      <Button color={Colors.primary} title={showDetails ? 'Hide Details' : 'Show Details'} onPress={() => {
+        setShowDetails(prevState => !prevState);
+      }} />
+      {showDetails && (
+        <View style={styles.detailItems}>
+          {props.items.map(cartItem => (
+            <CartItem
+              key={cartItem.productId}
+              quantity={cartItem.quantity}
+              amount={cartItem.sum}
+              title={cartItem.productTitle}
+            />
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -45,6 +61,9 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#888'
   },
+  detailItems: {
+    width: '100%',
+  }
 });
 
 export default OrderItem;
--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -10,7 +10,17 @@ const OrdersScreen = (props) => {
   const orders = useSelector(state => state.orders.orders);
 
   return (
-    <FlatList data={orders} keyExtractor={item => item.id} renderItem={itemData => <OrderItem amount={itemData.item.totalAmount} date={itemData.item.readableDate} />} />
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
+    />
   );
 };
 
