436a4338d631125f3e9cabebbb12f06bb1bc7e7a
Pawel Bak <pawelbak300589@gmail.com>
2021-04-20 21:04:56 +0100

    Reorganise product item component so it can be used on both products overview and user products screens

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image, Button, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
-
-import Colors from '../../constants/Colors';
+import { StyleSheet, View, Text, Image, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 
 const ProductItem = (props) => {
   let TouchableCmp = TouchableOpacity;
@@ -13,7 +11,7 @@ const ProductItem = (props) => {
   return (
     <View style={styles.product}>
       <View style={styles.touchable}>
-        <TouchableCmp onPress={props.onViewDetail} useForeground>
+        <TouchableCmp onPress={props.onSelect} useForeground>
           <View>
             <View style={styles.imageContainer}>
               <Image style={styles.image} source={{ uri: props.image }} />
@@ -23,8 +21,7 @@ const ProductItem = (props) => {
               <Text style={styles.price}>${props.price.toFixed(2)}</Text>
             </View>
             <View style={styles.actions}>
-              <Button color={Colors.primary} title="View Details" onPress={props.onViewDetail} />
-              <Button color={Colors.primary} title="To Cart" onPress={props.onAddToCart} />
+              {props.children}
             </View>
           </View>
         </TouchableCmp>
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { FlatList, Platform } from 'react-native';
+import { FlatList, Platform, Button } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import ProductItem from '../../components/shop/ProductItem';
 import HeaderButton from '../../components/UI/HeaderButton';
+
+import Colors from '../../constants/Colors';
 
 const UserProductsScreen = (props) => {
   const userProducts = useSelector(state => state.products.userProducts);
@@ -17,9 +19,11 @@ const UserProductsScreen = (props) => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onViewDetail={() => { }}
-          onAddToCart={() => { }}
-        />
+          onSelect={() => { }}
+        >
+          <Button color={Colors.primary} title="Edit" onPress={() => { }} />
+          <Button color={Colors.primary} title="Delete" onPress={() => { }} />
+        </ProductItem>
       )}
     />
   );
