c5521bdb1169a99adbac4f715ad8ef071884f866
Pawel Bak <pawelbak300589@gmail.com>
2021-04-21 20:34:46 +0100

    Refactor common shadow styling to Card component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image, TouchableOpacity, TouchableNativeFeedback, Platform } from 'react-native';
 
+import Card from '../../components/UI/Card';
+
 const ProductItem = (props) => {
   let TouchableCmp = TouchableOpacity;
 
@@ -9,7 +11,7 @@ const ProductItem = (props) => {
   }
 
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
         <TouchableCmp onPress={props.onSelect} useForeground>
           <View>
@@ -26,19 +28,12 @@ const ProductItem = (props) => {
           </View>
         </TouchableCmp>
       </View>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
     height: 300,
     margin: 20,
   },
