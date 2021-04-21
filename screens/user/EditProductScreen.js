b70660a5e42c29e7de0b567a14d3f5ca0694adab
Pawel Bak <pawelbak300589@gmail.com>
2021-04-21 18:52:15 +0100

    Add basic edit product screen

---
+++ screens/user/EditProductScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const EditProductScreen = (props) => {
+  return (
+    <View>
+      <Text>Edit product screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditProductScreen;
--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -12,6 +12,10 @@ const UserProductsScreen = (props) => {
   const userProducts = useSelector(state => state.products.userProducts);
   const dispatch = useDispatch();
 
+  const editProductHandler = (id) => {
+    props.navigation.navigate('EditProduct', { productId: id });
+  }
+
   return (
     <FlatList
       data={userProducts}
@@ -21,9 +25,11 @@ const UserProductsScreen = (props) => {
           image={itemData.item.imageUrl}
           title={itemData.item.title}
           price={itemData.item.price}
-          onSelect={() => { }}
+          onSelect={() => { editProductHandler(itemData.item.id); }}
         >
-          <Button color={Colors.primary} title="Edit" onPress={() => { }} />
+          <Button color={Colors.primary} title="Edit" onPress={() => {
+            editProductHandler(itemData.item.id);
+          }} />
           <Button color={Colors.primary} title="Delete" onPress={() => {
             dispatch(productsActions.deleteProduct(itemData.item.id));
           }} />
@@ -43,7 +49,15 @@ UserProductsScreen.navigationOptions = navData => {
             navData.navigation.toggleDrawer();
           }} />
       </HeaderButtons>
-    )
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Add' iconName={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+          onPress={() => {
+            navData.navigation.navigate('EditProduct');
+          }} />
+      </HeaderButtons>
+    ),
   }
 };
 
