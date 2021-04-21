42ad7413fb8057950ffe8eb31e46e0a5d78af9de
Pawel Bak <pawelbak300589@gmail.com>
2021-04-21 20:24:53 +0100

    Add alert before deleting a product

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, Platform, Button } from 'react-native';
+import { FlatList, Platform, Button, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -16,6 +16,17 @@ const UserProductsScreen = (props) => {
     props.navigation.navigate('EditProduct', { productId: id });
   }
 
+  const deleteHandler = (id) => {
+    Alert.alert('Are you sure?', 'Do you really want to delete this item?', [
+      { text: 'No', style: 'default' },
+      {
+        text: 'Yes', style: 'destructive', onPress: () => {
+          dispatch(productsActions.deleteProduct(id));
+        }
+      }
+    ])
+  };
+
   return (
     <FlatList
       data={userProducts}
@@ -30,9 +41,7 @@ const UserProductsScreen = (props) => {
           <Button color={Colors.primary} title="Edit" onPress={() => {
             editProductHandler(itemData.item.id);
           }} />
-          <Button color={Colors.primary} title="Delete" onPress={() => {
-            dispatch(productsActions.deleteProduct(itemData.item.id));
-          }} />
+          <Button color={Colors.primary} title="Delete" onPress={() => deleteHandler(itemData.item.id)} />
         </ProductItem>
       )}
     />
