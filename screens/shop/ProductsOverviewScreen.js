d8a768612af09e5184327e98b292a402f0996e64
Pawel Bak <pawelbak300589@gmail.com>
2021-04-27 19:13:21 +0100

    Update products overview screen to display loading spinner & to handle errors

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, FlatList, Platform, Button } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList, Platform, Button, View, Text, ActivityIndicator } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
@@ -10,12 +10,25 @@ import HeaderButton from '../../components/UI/HeaderButton';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = (props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   const products = useSelector(state => state.products.availableProducts);
   const dispatch = useDispatch();
 
+  const loadProducts = useCallback(async () => {
+    setError(null);
+    setIsLoading(true);
+    try {
+      await dispatch(productsActions.fetchProducts());
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
+  }, [dispatch, setIsLoading, setError]);
+
   useEffect(() => {
-    dispatch(productsActions.fetchProducts());
-  }, [dispatch]);
+    loadProducts();
+  }, [dispatch, loadProducts]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
@@ -23,6 +36,31 @@ const ProductsOverviewScreen = (props) => {
       productTitle: title
     })
   };
+
+  if (error) {
+    return (
+      <View style={styles.centered}>
+        <Text>An error occurred!</Text>
+        <Button title="Try again" onPress={loadProducts} color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.centered}>
+        <ActivityIndicator size="large" color={Colors.primary} />
+      </View>
+    );
+  }
+
+  if (!isLoading && products.length === 0) {
+    return (
+      <View style={styles.centered}>
+        <Text>No products found. Maybe start adding some!</Text>
+      </View>
+    );
+  }
 
   return (
     <FlatList
@@ -63,6 +101,12 @@ ProductsOverviewScreen.navigationOptions = navData => {
   }
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default ProductsOverviewScreen;
--- store/actions/products.js
+++ store/actions/products.js
@@ -7,23 +7,31 @@ export const SET_PRODUCTS = 'SET_PRODUCTS';
 
 export const fetchProducts = () => {
   return async dispatch => {
-    const response = await fetch('https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products.json');
+    try {
+      const response = await fetch('https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products.json');
 
-    const resData = await response.json();
-    const loadedProducts = [];
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
 
-    for (const key in resData) {
-      loadedProducts.push(new Product(
-        key,
-        'u1',
-        resData[key].title,
-        resData[key].imageUrl,
-        resData[key].description,
-        resData[key].price
-      ));
+      const resData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in resData) {
+        loadedProducts.push(new Product(
+          key,
+          'u1',
+          resData[key].title,
+          resData[key].imageUrl,
+          resData[key].description,
+          resData[key].price
+        ));
+      }
+
+      dispatch({ type: SET_PRODUCTS, products: loadedProducts });
+    } catch (err) {
+      throw err;
     }
-
-    dispatch({ type: SET_PRODUCTS, products: loadedProducts });
   };
 }
 
