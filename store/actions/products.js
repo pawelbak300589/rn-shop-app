81d004dd3e063314d116979fe87141a8cd74bbf2
Pawel Bak <pawelbak300589@gmail.com>
2021-04-27 19:49:34 +0100

    Handle errors and loading state on the edit product screen

--- store/actions/products.js
+++ store/actions/products.js
@@ -37,9 +37,14 @@ export const fetchProducts = () => {
 
 export const deleteProduct = (productId) => {
   return async dispatch => {
-    await fetch(`https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products/${productId}.json`, {
+    const response = await fetch(`https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products/${productId}.json`, {
       method: 'DELETE'
     });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
     dispatch({ type: DELETE_PRODUCT, pid: productId });
   };
 }
@@ -76,8 +81,7 @@ export const createProduct = (title, description, imageUrl, price) => {
 
 export const updateProduct = (id, title, description, imageUrl) => {
   return async dispatch => {
-
-    await fetch(`https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`, {
+    const response = await fetch(`https://rn-complete-guide-ecd74-default-rtdb.europe-west1.firebasedatabase.app/products/${id}.json`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json'
@@ -88,6 +92,10 @@ export const updateProduct = (id, title, description, imageUrl) => {
         imageUrl
       })
     });
+
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
 
     dispatch({
       type: UPDATE_PRODUCT,
